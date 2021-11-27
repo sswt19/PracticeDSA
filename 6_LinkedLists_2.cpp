165c543a6239734db576bfe37824060848d3d6ac
sswt19 <saswatanoop@gmail.com>
2021-11-27 13:44:51 +0530

    Create 6_LinkedLists_2.cpp
    Intersection of 2 lists
    check Palindrome in constant time

---
+++ 6_LinkedLists_2.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <sstream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+/*
+1.Find intersection point of Y LinkedList 
+2.isPalindrome using constant space
+*/
+
+// 1
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+{
+    auto p1 = headA, p2 = headB;
+    while (p1 || p2) //Both will become NULL at same time when we have traversed twice, distance covered a+b by both
+    {
+        if (p1 == p2)
+            return p1;
+        p1 = p1 ? p1->next : headB; //when p1 becomes NULL move it to second list
+        p2 = p2 ? p2->next : headA; // when p2 becomes NULL move it first list
+    }
+    return NULL; //not found the intersection
+}
+// 2
+class PalConstSpace
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+
+        bool isPal = true;
+        auto slow = head, fast = head;
+        while (fast && fast->next && fast->next->next)
+        { //when size 3 and 4 slow will be at 2
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *secondHalfOfList = reverse(slow->next); // everything after slow should be reversed and matche with the first half of list
+        auto t1 = head, t2 = secondHalfOfList;
+        while (t2) // since t1 size can be greater than t2 when list is odd in size
+        {
+            if (t1->val != t2->val)
+            {
+                isPal = false;
+                break;
+            }
+            t1 = t1->next;
+            t2 = t2->next;
+        }
+        slow->next = reverse(secondHalfOfList); // bring the list to original state again
+        return isPal;
+    }
+    ListNode *reverse(ListNode *head)
+    {
+        ListNode *prev = NULL, *curr = head;
+        while (curr)
+        {
+            auto next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
