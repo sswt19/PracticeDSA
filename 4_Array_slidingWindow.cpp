9e8b386291d7db749522dee63c5a0030fff3b284
sswt19 <saswatanoop@gmail.com>
2022-06-02 19:31:34 +0530

    Sliding Window
    2. Minimum Size Subarray Sum

--- 4_Array_slidingWindow.cpp
+++ 4_Array_slidingWindow.cpp
@@ -22,86 +22,77 @@ Sliding Window pattern:
 5. Longest Substring Without Repeating Characters: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 6.
 */
-// 1.
-class Solution
-{
-public:
-    double findMaxAverage(vector<int> &nums, int k)
-    {
-        double maxAvg = INT_MIN;
-        int winStart = 0, winEnd = 0, winSum = 0;
-        while (winEnd < nums.size())
-        {
-            winSum += nums[winEnd];         // use the new element who is part of window now
-            if (winEnd - winStart + 1 == k) // found a window of required size
-            {
-                maxAvg = max(maxAvg, winSum / (double)k); // process the window
-                // reduce the window size from left
-                winSum -= nums[winStart]; // remove the start of window content
-                winStart++;               // move the start of window ahead
-            }
-            winEnd++; // increase the window size from right
-        }
-        return maxAvg;
-    }
-};
 
-class Solution
+// 1
+double findMaxAverage(vector<int> &nums, int k)
 {
-public:
-    int minSubArrayLen(int target, vector<int> &nums)
+    double maxAvg = INT_MIN;
+    int winStart = 0, winEnd = 0, winSum = 0;
+    while (winEnd < nums.size())
     {
-        int minWin = INT_MAX;
-        int winS = 0, winE = 0, winSum = 0;
-
-        while (winE < nums.size())
+        winSum += nums[winEnd];         // use the new element who is part of window now
+        if (winEnd - winStart + 1 == k) // found a window of required size
         {
-            winSum += nums[winE];
-            while (winSum >= target) // current window might be the answer so we will keep reducing from left side till the condition is satisfied
-            {
-                minWin = min(minWin, winE - winS + 1);
-                // decrease the window from left side
-                winSum -= nums[winS];
-                winS++;
-            }
-            // increase the window from right side
-            winE++;
+            maxAvg = max(maxAvg, winSum / (double)k); // process the window
+            // reduce the window size from left
+            winSum -= nums[winStart]; // remove the start of window content
+            winStart++;               // move the start of window ahead
         }
-        return minWin == INT_MAX ? 0 : minWin;
+        winEnd++; // increase the window size from right
     }
-};
+    return maxAvg;
+}
+
+// 2. we will keep adding till the window sum>= target and then keep removing from left to find min window size possible
+int minSubArrayLen(int target, vector<int> &nums)
+{
+    int minWin = INT_MAX;
+    int winS = 0, winE = 0, winSum = 0;
+    while (winE < nums.size())
+    {
+        winSum += nums[winE];    // use the new element who is part of window now
+        while (winSum >= target) // we will keep reducing from left side till the condition is satisfied
+        {
+            minWin = min(minWin, winE - winS + 1);
+            // decrease the window from left side
+            winSum -= nums[winS];
+            winS++;
+        }
+        // now the window sum is < target so we will increase the window from right side
+        winE++;
+    }
+    return minWin == INT_MAX ? 0 : minWin; // if there was no window found return 0
+}
+
 // 3
-class Solution
+
+int lengthOfLongestSubstringKDistinct(string str, int k)
 {
-public:
-    int lengthOfLongestSubstringKDistinct(string str, int k)
+    int maxL = 0;
+    int winS = 0, winE = 0;
+    unordered_map<int, int> um;
+
+    while (winE < str.size())
     {
-        int maxL = 0;
-        int winS = 0, winE = 0;
-        unordered_map<int, int> um;
+        // Add to unordered_map to know the map size
+        if (um.find(str[winE]) == um.end())
+            um[str[winE]] = 1;
+        else
+            um[str[winE]] += 1;
 
-        while (winE < str.size())
+        while (um.size() > k) // we need to remove from left to make total types of characters = k
         {
-            // Add to unordered_map to know the map size
-            if (um.find(str[winE]) == um.end())
-                um[str[winE]] = 1;
-            else
-                um[str[winE]] += 1;
-
-            while (um.size() > k) // we need to remove from left to make total types of characters = k
-            {
-                um[str[winS]] -= 1;
-                if (um[str[winS]] == 0) // key does not exist delete it
-                    um.erase(str[winS]);
-                winS++;
-            }
-            maxL = max(maxL, winE - winS + 1);
-
-            winE++;
+            um[str[winS]] -= 1;
+            if (um[str[winS]] == 0) // key does not exist delete it
+                um.erase(str[winS]);
+            winS++;
         }
-        return maxL;
+        maxL = max(maxL, winE - winS + 1);
+
+        winE++;
     }
-};
+    return maxL;
+}
 
 // 4
 class Solution
